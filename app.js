1470ce1fd7865731b02851c9cf8552a86c63335e
DeenLennon <github.com/DeenLennon>
2023-09-14 14:11:57 +0100

    The js_file

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const currencyFirstEl = document.getElementById("currency_first");
-const worthFirstEl = document.getElementById("worth_first");
-const currencySecondEl = document.getElementById("currency_second");
-const worthSecondEl = document.getElementById("worth_second");
+const currencyEl = document.getElementById("currency");
+const amountEl = document.getElementById("amount");
+const otherCurrencyEl = document.getElementById("other_currency");
+const worthEl = document.getElementById("worth");
 const exchangeRateEl = document.getElementById("exchange_rate");
 
 updateRate()
 
 function updateRate() {
   fetch(
-    `https://v6.exchangerate-api.com/v6/e0947f78e9fe06055d1b51a2/latest/${currencyFirstEl.value}`
+    `https://v6.exchangerate-api.com/v6/e0947f78e9fe06055d1b51a2/latest/${currencyEl.value}`
   )
     .then((res) => res.json())
-    .then((data) => {const rate = data.conversion_rates[currencySecondEl.value];
-        console.log(data);
-        exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
-        worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2)
+    .then((data) => {const rate = data.conversion_rates[otherCurrencyEl.value];
+        exchangeRateEl.innerText = `1 ${currencyEl.value} = ${rate + " " + otherCurrencyEl.value}`;
+        worthEl.value = (amountEl.value * rate).toFixed(2)
     });
 }
 
-currencyFirstEl.addEventListener("change", updateRate);
+currencyEl.addEventListener("change", updateRate);
 
-currencySecondEl.addEventListener("change", updateRate);
+otherCurrencyEl.addEventListener("change", updateRate);
 
-worthFirstEl.addEventListener("input", updateRate);
+amountEl.addEventListener("input", updateRate);
 
